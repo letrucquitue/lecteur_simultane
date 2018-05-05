b548e370e28730fbd2ad30e9bb44c49b519a5c31
Unknown <leobloum@gmail.com>
2018-05-04 21:33:02 -0400

    clean MainActivity

--- app/src/main/java/ca/ulaval/ima/mp/MainActivity.java
+++ app/src/main/java/ca/ulaval/ima/mp/MainActivity.java
@@ -29,48 +29,19 @@ import ca.ulaval.ima.mp.service.BluetoothService;
 public class MainActivity extends AppCompatActivity implements OnListFragmentInteractionListener{
 
     private BottomNavigationView navigation;
-    private ArrayList<Fragment> fragments;
     private Fragment current_fragment;
-    private Fragment choose_fragment;
-    private Fragment properties_fragment;
-    private Fragment connection_fragment;
-    private Fragment play_fragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Init navigation
         navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
-        /*
-        fragments = new ArrayList<>();
-        fragments.add(new ChooseVideoFragment());
-        fragments.add(new PropertiesFragment());
-        fragments.add(new ConnectionFragment());
-        fragments.add(new PlayFragment());
-        */
-
-        //FRAGMENT
-        choose_fragment = new ChooseVideoFragment();
-        properties_fragment = new PropertiesFragment();
-        connection_fragment = new ConnectionFragment();
-        play_fragment = new PlayFragment();
-        current_fragment = choose_fragment;
-
         //DEFAULT FRAGMENT
-        /*FragmentManager fragmentManager = getFragmentManager();
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.add(R.id.main_content,properties_fragment,"properties_fragment");
-        fragmentTransaction.hide(properties_fragment);
-        fragmentTransaction.add(R.id.main_content,connection_fragment,"connection_fragment");
-        fragmentTransaction.hide(connection_fragment);
-        fragmentTransaction.add(R.id.main_content,choose_fragment,"choose_fragment");
-        fragmentTransaction.hide(choose_fragment);
-        fragmentTransaction.show(current_fragment);
-        fragmentTransaction.commit();*/
-        //fragmentTransaction.replace(R.id.main_content, current_fragment).commit();
+        current_fragment = new ChooseVideoFragment();
         FragmentManager fragmentManager = getFragmentManager();
         fragmentManager.beginTransaction()
                 .replace(R.id.main_content, current_fragment)
@@ -87,62 +58,34 @@ public class MainActivity extends AppCompatActivity implements OnListFragmentInt
             FragmentManager fragmentManager = getFragmentManager();
             Fragment new_fragment = null;
 
+            //choix du fragment selon onglet
             switch (item.getItemId()) {
                 case R.id.navigation_choose:
-                    //new_fragment = choose_fragment;
                     new_fragment = new ChooseVideoFragment();
-                    //new_fragment = (ChooseVideoFragment) getFragmentManager().findFragmentByTag("choose_fragment");
                     break;
                 case R.id.navigation_properties:
-                    //new_fragment = properties_fragment;
                     new_fragment = new PropertiesFragment();
-                    //new_fragment = (PropertiesFragment) getFragmentManager().findFragmentByTag("properties_fragment");
                     break;
                 case R.id.navigation_connection:
-                    //new_fragment = connection_fragment;
                     new_fragment = new ConnectionFragment();
-                    //new_fragment = (ConnectionFragment) getFragmentManager().findFragmentByTag("connection_fragment");
                     break;
                 case R.id.navigation_play:
                     Intent myIntent = new Intent(getBaseContext(), PlayVideoActivity.class);
                     startActivity(myIntent);
-                    //new_fragment = new PlayFragment();
                     return true;
             }
             if(new_fragment != null && new_fragment.getClass() != current_fragment.getClass()){
-                //on détruit l'ancien fragment
-                //current_fragment.onDestroy();
-                //fragmentManager.beginTransaction().hide(current_fragment).commit();
+                //on remplace le fragment
                 current_fragment = new_fragment;
                 fragmentManager.beginTransaction().replace(R.id.main_content, new_fragment).commit();
-                //fragmentManager.beginTransaction().show(new_fragment).commit();
-                /*FragmentManager fm = getFragmentManager();
-                fm.beginTransaction()
-                        .setCustomAnimations(android.R.animator.fade_in, android.R.animator.fade_out)
-                        .show(new_fragment)
-                        .commit();*/
                 return true;
             }
             return false;
         }
     };
 
-    public BottomNavigationView getNavigationBottomView(){
-        return this.navigation;
-    }
-
     @Override
     public void onListFragmentInteraction(BluetoothDevices.BluetoothItem item) {
 
-    }
-
-    public interface OnListFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onListFragmentInteraction(BluetoothDevices.BluetoothItem item);
-    }
-
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
     }
 }
