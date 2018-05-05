d0283a4960f11f2f8ab322efbc58868360b29ba1
Unknown <leobloum@gmail.com>
2018-05-04 20:39:09 -0400

    activer BT et rendre visible automatiquement

--- app/src/main/java/ca/ulaval/ima/mp/fragment/ConnectionFragment.java
+++ app/src/main/java/ca/ulaval/ima/mp/fragment/ConnectionFragment.java
@@ -3,7 +3,9 @@ package ca.ulaval.ima.mp.fragment;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.bluetooth.BluetoothAdapter;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -12,6 +14,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Set;
@@ -32,6 +35,24 @@ public class ConnectionFragment extends Fragment{
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        //Activer le bluetooth
+        BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (mBluetoothAdapter.isEnabled() == false) {
+            mBluetoothAdapter.enable();
+        }
+
+        //Rendre l'appareil visible
+        try{
+            Intent discoverableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+            discoverableIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 300);
+            startActivity(discoverableIntent);
+        }
+        catch(Exception e){
+            Toast.makeText(getActivity().getApplicationContext(), "Impossible de vous rendre decouvrable", Toast.LENGTH_LONG).show();
+            Log.d("Erreur", "Impossible detre decouvrable");
+        }
+
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_connection, container, false);
         btnInviteNotPair = view.findViewById(R.id.buttonInviteNotPair);
@@ -66,6 +87,7 @@ public class ConnectionFragment extends Fragment{
                 //TODO : Associer le video choisi a la classe SelfUser
             }
         });
+
         return view;
     }
 
